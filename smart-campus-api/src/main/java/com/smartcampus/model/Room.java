f4561b5035b4bb463bba8c814700069001ccc78e
ishara20231306-coder <ishara.20231306@iit.ac.lk>
2026-04-20 14:12:15 +0530

    done Room.java

--- smart-campus-api/src/main/java/com/smartcampus/model/Room.java
+++ smart-campus-api/src/main/java/com/smartcampus/model/Room.java
@@ -15,4 +15,32 @@ public class Room {
         this.capacity = capacity;
     }
 
+    public String getId(){
+        return id;
+    }
+    public void setId(String id){
+        this.id = id;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public int getCapacity(){
+        return capacity;
+    }
+    public void setCapacity(int capacity){
+        this.capacity = capacity;
+    }
+
+    public List<Sensor> getSensors(){
+        return sensors;
+    }
+    public void setSensors(){
+        this.sensors = sensors;
+    }
+
 }
