c632e94f389967f6e53c6d804846e6e82b8ec32e
ishara20231306-coder <ishara.20231306@iit.ac.lk>
2026-04-20 14:24:24 +0530

    initialized sensor.java file

--- smart-campus-api/src/main/java/com/smartcampus/model/Sensor.java
+++ smart-campus-api/src/main/java/com/smartcampus/model/Sensor.java
@@ -1,5 +1,55 @@
 package com.smartcampus.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Sensor{
-    
+    private String id;
+    private String type;
+    private String status = "ACTIVE";
+    private double currentValue;
+    private List<Reading> readings = new ArrayList<>();
+
+
+
+    public Sensor(String id, String type) {
+        this.id = id;
+        this.type = type;
+    }
+
+    public String getId() { 
+        return id; 
+    }
+    public void setId(String id) { 
+        this.id = id; 
+    }
+
+    public String getType() { 
+        return type; 
+    }
+    public void setType(String type) { 
+        this.type = type; 
+    }
+
+    public double getCurrentValue() { 
+        return currentValue; 
+    }
+    public void setCurrentValue(double currentValue) { 
+        this.currentValue = currentValue; 
+    }
+
+    public String getStatus() { 
+        return status; 
+    }
+    public void setStatus(String status) { 
+        this.status = status; 
+    }
+
+    public List<Reading> getReadings() { 
+        return readings; 
+    }
+    public void setReadings(List<Reading> readings) { 
+        this.readings = readings; 
+    }
+
 }
